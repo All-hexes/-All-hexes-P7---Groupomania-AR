f5fa5987773967b35ec23e80cfca21f09b08f03c
Alexandre Roblain <alexrob260@gmail.com>
2022-09-28 00:32:57 +0200

    correction app.js, server.js

--- back/server.js
+++ back/server.js
@@ -1,55 +1,47 @@
-require('dotenv').config({ path: '.env' });
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
-const sauceRoutes = require('./routes/sauce');
-const path = require('path');
-const cors = require('cors');
-const helmet = require('helmet');
-const { DB_USER, DB_SERVER, DB_PASSWORD } = process.env;
+const http = require('http');
+const app = require('./app');
 
-const app = express();
+const normalizePort = val => {
+    const port = parseInt(val, 10);
 
-app.use(helmet({
-    crossOriginResourcePolicy: { policy: "same-site" }
-}));
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-app.use(express.json());
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
 
-mongoose
-    .connect(
-        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_SERVER}/?retryWrites=true&w=majority`,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    )
-    .then(() => console.log("MongoDB connecté !"))
-    .catch(() => console.log(("Connexion à MongoDB échouée. ")));
+const server = http.createServer(app);
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
-app.use(cors());
-
-app.use('/api/auth', userRoutes);
-
-app.use('/api/sauces', sauceRoutes);
-
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.get("*", (req, res) => {
-    res.status(404).send({ message: "Not found !" })
-});
-
-module.exports = app;
+server.listen(port);
